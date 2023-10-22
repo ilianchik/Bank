5c79d97195f4a7a21a81abca9cd4e16609341570
Ilia Kozachyshyn <kozachishin543@gmail.com>
2023-10-22 18:26:40 +0300

    v-3

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const displayMovements = function (movements) {
     const html = ` 
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}€</div>
   </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
@@ -90,11 +90,25 @@ console.log(accounts);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 
 calcDisplayBalance(account1.movements);
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements.filter(e => e > 0).reduce((acc, e) => acc + e, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements.filter(e => e < 0).reduce((acc, e) => acc + e, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(e => e > 0)
+    .map(e => (e * 1.2) / 100)
+    .reduce((acc, e) => acc + e, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
 ///////
 
 // const calcAverageHumanAge = function (ages) {
